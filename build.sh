8b02dad73cc20c262eb31348c5b1aaaf095c7052
cilegordev <deputyalectro@icloud.com>
2025-04-12 17:27:11 +0700

    commit

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ sudo rsync -aAXv /media/user/lfs /media/user/ext-ssd/lfs \
 sudo mksquashfs /media/user/ext-ssd/lfs /media/filesystem.squashfs -comp xz -e boot
 
 sudo cp -v /media/filesystem.squashfs ~/simple-live-iso/filesystem.squashfs
-sudo cp /media/user/lfs/boot/vmlinuz-$(uname -r) ~/simple-live-iso/vmlinuz
-sudo cp /media/user/lfs/boot/initrd.img-$(uname -r) ~/simple-live-iso/initramfs
+sudo cp -v /media/user/lfs/boot/vmlinuz-$(uname -r) ~/simple-live-iso/vmlinuz
+sudo cp -v /media/user/lfs/boot/initrd.img-$(uname -r) ~/simple-live-iso/initrd
 
 sudo ~/simple-live-iso/boot.sh
 
-sudo grub-mkrescue -o /media/user/ext-ssd/lfs/live-os.iso ~/simple-live-iso
+sudo grub-mkrescue ~/simple-live-iso -o /media/user/ext-ssd/linux-live.iso
